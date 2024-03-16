ef65dc042fc1e684833f62560d10a545c7b5a12a
lingulist <mattis.list@lingulist.de>
2024-03-16 21:49:52 +0100

    update

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from pyedictor import fetch
 from lingpy import Wordlist
-
+import json
 
 wl = fetch(
         "fanqie",
@@ -9,3 +9,25 @@ wl = fetch(
         to_lingpy=Wordlist)
 
 wl.output('tsv', filename="fanqie", ignore="all", prettify=False)
+
+data = {"shang": {"baxter": {}, "zhou": {}}, "xia": {"baxter": {}, "zhou": {}}}
+for idx in wl:
+    
+    who = wl[idx, "doculect"].lower()
+    what = "shang" if "shang" in wl[idx, "concept"] else "xia"
+    char = wl[idx, "concept"].split(" ")[0]
+    tokens = wl[idx, "tokens"]
+    
+
+    if what == "shang" and len(tokens) == 2:
+        data[what][who][char] = list(tokens)
+    elif what == "shang" and len(tokens) != 2:
+        print(what, who, char, tokens)
+    
+    if what == "xia" and len(tokens) == 4:
+        data[what][who][char] = list(tokens)
+    elif what == "xia" and len(tokens) != 4:
+        print(what, who, char, tokens)
+
+with open("app/data.js", "w") as f:
+    f.write("var fanqie = "+json.dumps(data)+";")
